0a9471a4c87655ae9ecea089e682ae0bf066abd1
Ashley <waifushork@gmail.com>
2021-03-02 17:36:46 -0500

    added Compiler Service

---
+++ src/CompilerService/Compiler.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace VivianSandbox.CompilerService
+{
+    public class Compiler
+    {
+        public async Task<string> CompileFile()
+        {
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    // todo: setup project files inside docker instance
+                    FileName = "dotnet.exe", 
+                    Arguments = @"run --project G:\VivianLang\Vivian\samples\src\Playground.vivproj",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                }
+            };
+        
+            process.Start();
+            var output = await process.StandardOutput.ReadToEndAsync();
+
+            await Task.CompletedTask;
+            return output;
+        }
+    }
+}
